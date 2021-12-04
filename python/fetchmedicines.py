62744f2478b0cfcbae21147df12c3f9ff752543d
anilpotru <94579808+anilpotru@users.noreply.github.com>
2021-12-05 01:26:35 +0530

    fetch movies python code

--- python/fetchmedicines.py
+++ python/fetchmedicines.py
@@ -1,5 +1,8 @@
 import mysql.connector
 
+'''
+connect to the mysql database with database details
+'''
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
@@ -9,25 +12,31 @@ mydb = mysql.connector.connect(
 )
 
 mycursor = mydb.cursor()
-
+'''
+query to execute to fetch medicinestock
+'''
 mycursor.execute("SELECT * FROM MedicineStock")
 
 myresult = mycursor.fetchall()
 
 def convertTuple(tup):
     st = ','.join(map(str, tup))
-    return st
+    return st  
+	
 print('The available medicines are')
 print('@@@@@@@@@@@@@@@@@')
+'''
+printing the medical stock in console
+'''
 for x in myresult:
   print(x)
-  
 print('@@@@@@@@@@@@@@@@@')
 print('saving the medicines list to the medicinestock_out.txt')
-
+'''
+saving the medicinestock to the txt file
+'''
 with open('medicinestock_out.txt', 'w') as f:
     for line in myresult:
         f.write(convertTuple(line))
         f.write('\n')
-
 print('successfully saved the medicinestock_out.txt file');
